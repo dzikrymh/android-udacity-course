ff29f6582983a6e5b0c10d54a78ba10cda564193
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2021-12-15 10:18:26 +0700

    add data binding & create project ColorMyView

--- AboutMe/app/src/main/java/me/dzikry/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/me/dzikry/aboutme/MainActivity.kt
@@ -1,11 +1,48 @@
 package me.dzikry.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import me.dzikry.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Dzikry Maulana Hakim")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+//        findViewById<Button>(R.id.done_button).setOnClickListener {
+//            addNickname(it)
+//        }
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(it: View) {
+        binding.apply {
+//            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            it.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(it.windowToken, 0)
     }
 }
